a086a158fd51c2ad608335b2afc28e0968df4f49
zhaodaniel <148019206+zhaodaniel@users.noreply.github.com>
2025-03-14 20:04:47 +0000

    Fixed previous CellTest and PlayerTest placements and added PlayerTest

--- test/cs3500/pawnsboard/CardTest.java
+++ test/cs3500/pawnsboard/CardTest.java
@@ -55,4 +55,53 @@ class CardTest {
     int[][] influenceGrid = new int[4][4];  // Not 5x5
     new Card("Card1", 2, 10, influenceGrid);
   }
+
+  @Test
+  public void testCardGetName() {
+    influenceGrid = new int[5][5];
+    card = new Card("Card1", 2, 10, influenceGrid);
+    assertEquals("Card1", card.getName());
+  }
+
+  @Test
+  public void testCardGetCost() {
+    influenceGrid = new int[5][5];
+    card = new Card("Card1", 2, 10, influenceGrid);
+    assertEquals(2, card.getCost());
+  }
+
+  @Test
+  public void testCardGetValue() {
+    influenceGrid = new int[5][5];
+    card = new Card("Card1", 2, 10, influenceGrid);
+    assertEquals(10, card.getValue());
+  }
+
+  @Test
+  public void testCardGetInfluenceGrid() {
+    influenceGrid = new int[5][5];
+    card = new Card("Card1", 2, 10, influenceGrid);
+    assertEquals(influenceGrid, card.getInfluenceGrid());
+  }
+
+  @Test
+  public void testCardEquals() {
+    influenceGrid = new int[5][5];
+    card = new Card("Card1", 2, 10, influenceGrid);
+    Card card2 = new Card("Card1", 2, 10, influenceGrid);
+    Card card3 = new Card("Card2", 3, 15, influenceGrid);
+
+    assertEquals(true, card.equals(card));
+    assertEquals(true, card.equals(card2));
+    assertEquals(false, card.equals(null));
+    assertEquals(false, card.equals(influenceGrid));
+    assertEquals(false, card.equals(card3));
+  }
+
+  @Test
+  public void testCardToString() {
+    influenceGrid = new int[5][5];
+    card = new Card("Card1", 2, 10, influenceGrid);
+    assertEquals("Card1 (Cost: 2, Value: 10)", card.toString());
+  }
 }
